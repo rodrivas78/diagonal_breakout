ff80b56b764f18a8a0f92df908b980d647f9a820
Rodrigo Gonzales <rrivas2009@gmail.comm>
2024-06-04 22:39:47 -0300

    initial commit

---
+++ scenes/bola/bola.gd
@@ -0,0 +1,101 @@
+extends Area2D
+
+
+# Referências Gerais
+@onready var timer_da_bola : Timer = $TimerDaBola
+@onready var som_impacto_bloco : AudioStreamPlayer = $SomImpactoBloco
+@onready var som_impacto_paddle : AudioStreamPlayer = $SomImpactoPaddle
+@onready var som_impacto_tela : AudioStreamPlayer = $SomImpactoTela
+
+# Movimento da Bola
+var velocidade_da_bola : float = 400.0
+var posicao_inicial : Vector2 = Vector2(400, 500)
+var direcao_inicial : Vector2 = Vector2(0, 0)
+var nova_direcao : Vector2 = Vector2(0, 0)
+
+# Limites da Bola
+var x_minimo : float = 0
+var x_maximo : float = 800
+var y_minimo : float = 0
+var y_maximo : float = 600
+
+# Verificações
+var primeiro_lancamento : bool = true
+var caiu_da_tela : bool = false
+
+
+func _ready():
+	timer_da_bola.one_shot = true
+	resetar_bola()
+
+
+func _process(delta):
+	# Se for o primeiro lançamento, esperar a ação do Jogador para lançar
+	if primeiro_lancamento:
+		if Input.is_action_just_pressed("lancar-bola"):
+			escolher_direcao_inicial()
+			primeiro_lancamento = false
+			
+	movimentar_bola(delta)
+	verificar_posicao_da_bola()
+	
+	
+func resetar_bola() -> void:
+	# Posiciona a Bola acima do Paddle
+	position = posicao_inicial
+	
+
+func escolher_direcao_inicial() -> void:
+	# Escolhe uma nova direção Horizontal
+	var x_aleatorio = [-1, 1].pick_random()
+	
+	# Aplica a nova direção
+	direcao_inicial = Vector2(x_aleatorio, -1)
+	nova_direcao = direcao_inicial
+	
+
+func movimentar_bola(delta : float) -> void:
+	# Movimenta a Bola com base em sua nova direção
+	position += nova_direcao * velocidade_da_bola * delta
+	
+
+func verificar_posicao_da_bola() -> void:
+	# Se a Bola estiver dentro da tela, a rebate ao colidir com as bordas
+	if position.y <= y_maximo:
+		if position.y <= y_minimo:
+			som_impacto_tela.play()
+			nova_direcao.y *= -1
+		
+		if position.x <= x_minimo or position.x >= x_maximo:
+			som_impacto_tela.play()
+			nova_direcao.x *= -1
+	
+	# Se a Bola cair da tela
+	if position.y > y_maximo and not caiu_da_tela:
+		timer_da_bola.start()
+		caiu_da_tela = true
+
+
+func sair_da_tela() -> void:
+	# Para o movimento da Bola e reseta sua posição
+	nova_direcao = Vector2(0, 0)
+	primeiro_lancamento = true
+	resetar_bola()
+
+
+func _on_body_entered(body):
+	# Se colidir com o Paddle, rebate
+	if body.is_in_group("paddle"):
+		som_impacto_paddle.play()
+		nova_direcao.y *= -1	
+	
+	# Se colidir com o Bloco, desconta sua vida e rebate
+	elif body.is_in_group("blocos"):
+		som_impacto_bloco.play()
+		body.receber_dano()
+		nova_direcao.y *= -1
+
+
+func _on_timer_da_bola_timeout():
+	sair_da_tela()
+	caiu_da_tela = false	
---
+++ scenes/game_manager/game_manager.gd
@@ -0,0 +1,96 @@
+extends Node2D
+
+
+# Controle dos Blocos
+@export_group("Controle dos Blocos")
+@export var blocos : Node2D
+var blocos_na_fase : int = 0
+
+# Passar de Fase
+@export_group("Passar de Fase")
+@export var proxima_fase : String
+@onready var timer_do_passar_de_fase : Timer = $TimerDoPassarDeFase
+
+#Controle dos bumpers
+@export_group("Controle dos Bumpers")
+@export var diagonalA : Node2D
+@export var diagonalB : Node2D
+@export var jump_positions = [Vector2(100, 200), Vector2(300, 200), Vector2(500, 200)]
+var current_jump_index = 0
+
+# Limites da Bola
+#x_minimo : float = 0
+var x_offset : float = 100.0
+#var y_minimo : float = 0
+var y_offset : float = 100.0
+var newPosition : float = 0.0
+
+# Make the object visible
+#meu_objeto.deactivate()
+#my_object.activate()
+
+func _ready():
+	buscar_blocos()
+
+
+func _process(delta):
+	receber_inputs()
+	
+
+func receber_inputs() -> void:
+	# Reinicia a fase
+	if Input.is_action_just_pressed("reiniciar"):
+		get_tree().reload_current_scene()
+	# Sai do jogo
+	if Input.is_action_just_pressed("sair"):
+		get_tree().quit()
+	#alterna rebatedores
+	# Verifique se a tecla "espaço" foi pressionada
+	if Input.is_action_just_pressed("shift-paddle"):
+		diagonalA.visible = !diagonalA.visible
+		diagonalB.visible = !diagonalB.visible
+		
+	# movimenta os paddles
+	if Input.is_action_just_pressed("mv-esquerdo"):
+		if current_jump_index < jump_positions.size():
+			diagonalA.position = jump_positions[current_jump_index]
+			current_jump_index += 1
+		
+	elif Input.is_action_pressed("mv-direito"):
+		x_offset = x_offset + 100
+		print(x_offset)
+		diagonalA.position = Vector2(x_offset, 200)
+		
+	#TODO - Definir a posição inicial dos paddles
+	#TODO - e movimentos para cima e para baixo. 
+	
+func buscar_blocos() -> void:
+	# Conta quantos Blocos há na fase
+	for bloco in blocos.get_children():
+		blocos_na_fase += 1
+
+
+func atualizar_contagem_dos_blocos() -> void:
+	# Remove um Bloco da contagem e, SE não tiver mais nenhum, inicia o passar de fase
+	blocos_na_fase -= 1
+	if blocos_na_fase <= 0:
+		timer_do_passar_de_fase.start()
+
+
+func _on_timer_do_passar_de_fase_timeout():
+	# Carrega a próxima fase
+	get_tree().change_scene_to_file(proxima_fase)
+
+# Posiciona o objeto na próxima posição de destino
+#func jump_to_next_position():
+	## Verifica se há mais posições de destino
+	#if current_jump_index < jump_positions.size():
+		## Move o objeto para a nova posição
+		#position = jump_positions[current_jump_index]
+		#current_jump_index += 1
+	#else:
+		## Volta para a primeira posição
+		#current_jump_index = 0
+		#position = jump_positions[current_jump_index]
+	
+		
